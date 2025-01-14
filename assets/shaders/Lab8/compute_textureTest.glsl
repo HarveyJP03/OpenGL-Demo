50d3708aac752ef88b7512c83b2c73a6ec381562
HarveyJP03 <harvey.palmer@live.co.uk>
2025-01-14 16:56:57 +0000

    Checker bboard with compute shader

--- assets/shaders/Lab8/compute_textureTest.glsl
+++ assets/shaders/Lab8/compute_textureTest.glsl
@@ -6,6 +6,11 @@ layout(binding = 0, rgba8) uniform image2D outputImg;
 void main()
 {
 	ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy); //ID of the work item(thread) in relation to the whole dispatch, not just the work group
-	vec4 pixelCol = vec4(0.5, 0.5, 1.0, 1.0);
+	vec4 pixelCol = vec4(0.0, 0.0, 0.0, 1.0);
+
+	if((gl_WorkGroupID.x + gl_WorkGroupID.y) % 2 == 0  )
+	{
+		pixelCol = vec4(1.0, 1.0, 1.0, 1.0);
+	}
 	imageStore(outputImg, pixel_coords, pixelCol); //write color to image at pixel coords
 }
